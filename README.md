b38574b55da86f77a6a60ef547d5dfc1aec03c10
Sangjun Lee <jjojjosj@gmail.com>
2023-01-29 22:55:15 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 노마드코더 - React JS 마스터클래스
+
+이 프로젝트는 <https://nomadcoders.co/react-masterclass> 의 강의를 수강하면서 만드는 프로젝트이다.
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+function App() {
+  return (
+    <Father>
+      <BoxOne />
+      <BoxTwo />
+    </Father>
+  );
+}
+
+export default App;
