d378f34954f87ea7f6d701cc23bc2728c6752153
Sangjun Lee <jjojjosj@gmail.com>
2023-02-02 21:49:03 +0900

    add coinId parameter

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom";
 
+interface RouteParams {
+  coinId: string;
+}
 function Coin() {
-  const params = useParams();
-  return <h1>Coin</h1>;
+  const { coinId } = useParams<RouteParams>();
+
+  return <h1>Coin: {coinId}</h1>;
 }
 export default Coin;
